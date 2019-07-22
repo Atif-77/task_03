40e376a60a535dbe70fc0039f00c75964ff7ab72
Atif-77 <atif.shehrim@gmail.com>
2019-07-22 21:18:52 +0300

    Task_03 Done

--- restaurants/views.py
+++ restaurants/views.py
@@ -7,6 +7,14 @@ def restaurant_list(request):
 
     context = {
 
+    "my_list": [
+       {"restaurant_name": "krm", "food_type": "gril"},
+
+       {"restaurant_name": "shbra", "food_type": "kbsh"},
+
+       {"restaurant_name": "sndbad", "food_type": "shawrma"},
+    ]
+
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +22,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+
+    	"my_object": [
+    		{"restaurant_name": "shbra", "food_type": "kbsh"},    	
+    		]
 
     }
     return render(request, 'detail.html', context)
